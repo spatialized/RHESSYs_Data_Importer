a573d3b830bf9310decc7c89f50dc3c620d65b78
David Gordon <dgordon412@gmail.com>
2025-11-02 00:01:57 -0700

    Update RHESSYsDAL.cs

--- RHESSYs_Data_Importer/DAL/RHESSYsDAL.cs
+++ RHESSYs_Data_Importer/DAL/RHESSYsDAL.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using RHESSYs_Data_Importer.Models;
+using RHESSYs_Data_Importer.Models.RHESSYs_Data_Importer.Models;
 
 namespace RHESSYs_Data_Importer.DAL
 {
     public class RHESSYsDAL
     {
+        private bool useMySQL = true;
+
+        const string mySqlConnectionString = "";
+
         /// <summary>
         /// Add data point to FutureMountain cubedata table
         /// </summary>
@@ -37,6 +43,93 @@ namespace RHESSYs_Data_Importer.DAL
                     return true;
                 else
                     return false;
+            }
+        }
+
+
+        /// <summary>
+        /// Add data point to FutureMountain dates table
+        /// </summary>
+        public bool AddWaterDataFrame(WaterDataFrame frame)
+        {
+            using (var db = new WaterDataDbContext())
+            {
+                db.WaterData.Add(frame);
+                if (db.SaveChanges() > 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        
+        /// <summary>
+        /// Add data point to FutureMountain dates table
+        /// </summary>
+        public bool AddPatchData(int patchId, PatchPointCollection frame)
+        {
+            using (var db = new PatchDataDbContext())
+            {
+                PatchDataRecord record = ConvertPatchPointCollectionToRecord(frame, patchId);
+                db.PatchData.Add(record);
+                if (db.SaveChanges() > 0)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        private PatchDataRecord ConvertPatchPointCollectionToRecord(PatchPointCollection frame, int patchId)
+        {
+            PatchDataRecord record = new PatchDataRecord();
+            record.patchID = patchId;
+            record.SetData(frame);
+            return record;
+        }
+
+        /// <summary>
+        /// Add data point to FutureMountain dates table
+        /// </summary>
+        public bool AddTerrainDataFrame(TerrainDataFrameJSONRecord frame)
+        {
+            try
+            {
+                using (var db = new TerrainDataDbContext())
+                {
+                    db.TerrainData.Add(frame);
+                    if (db.SaveChanges() > 0)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: ex:" + ex.Message);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Add data point to FutureMountain dates table
+        /// </summary>
+        public bool AddFireDataFrame(FireDataFrameJSONRecord frame)
+        {
+            try
+            {
+                using (var db = new FireDataDbContext())
+                {
+                    db.FireData.Add(frame);
+                    if (db.SaveChanges() > 0)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: ex:" + ex.Message);
+                return false;
             }
         }
     }
